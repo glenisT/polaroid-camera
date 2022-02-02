00d6a4e3517a42dc2b26acc6ea6d5baedd16e345
g.tivari <g.tivari@sogetel.it>
2022-02-02 17:38:25 +0100

    Make animation functional.

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var button = document.getElementById("snapper");
 
 button.addEventListener("click", function(){
     console.log("CLICKED!");
-    document.body.style.animation = "flash 0.5s";
+    document.body.parentNode.style.animation = "flash 0.5s";
 });
