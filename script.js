4186d3bc4e853b6fcf56482b435a2807b6e14152
g.tivari <g.tivari@sogetel.it>
2022-02-02 17:51:51 +0100

    Add invisible polaroid.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var button = document.getElementById("snapper");
+var camera = document.getElementById("camera");
+var polaroid = document.getElementById("polaroid");
 
 button.addEventListener("click", function(){
     console.log("CLICKED!");
     document.body.parentNode.style.animation = "flash 0.5s";
+    
 });
