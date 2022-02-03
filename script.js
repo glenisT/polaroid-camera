29b848141b12d564521378e9f71d7b566747789a
g.tivari <g.tivari@sogetel.it>
2022-02-03 09:45:46 +0100

    Add animationend event listener.

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ var polaroid = document.getElementById("polaroid");
 button.addEventListener("click", function(){
     console.log("CLICKED!");
     document.body.parentNode.style.animation = "flash 0.5s";
-    
+});
+
+document.body.parentNode.addEventListener("animationend", function(){
+    console.log("ended!");
+    polaroid.style.animation = "print 0.5s 1 forwards";
 });
