f5c95ce604e1ae550434c8b04d5b8e90cb2eeebb
g.tivari <g.tivari@sogetel.it>
2022-02-03 10:25:36 +0100

    Add print animation to js.

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ var picture = document.getElementById("pic");
 button.addEventListener("click", function(){
     console.log("CLICKED!");
     document.body.parentNode.style.animation = "flash 0.5s";
+    camera.style.animation = "print 1s 1.5s 1 forwards";
 });
 
-//after flash print polaroid
-camera.addEventListener("animationend", function(){
-    console.log("ended!");
-    camera.style.animation = "print 0.5s 1 forwards";
-    // picture.style.animation = "develop 3s forwards";
-});
+// //after flash print polaroid
+// camera.addEventListener("animationend", function(){
+//     console.log("ended!");
+//     camera.style.animation = "print 0.5s 1 forwards";
+//     // picture.style.animation = "develop 3s forwards";
+// });
