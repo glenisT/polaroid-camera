0294b47c1e419af23752848d3673d175be8fda0e
g.tivari <g.tivari@sogetel.it>
2022-02-03 10:12:58 +0100

    Remove polaroid from camera parenting and match shake&develop animation timings.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var button = document.getElementById("snapper");
 var camera = document.getElementById("camera");
 var polaroid = document.getElementById("polaroid");
+var picture = document.getElementById("pic");
 
 button.addEventListener("click", function(){
     console.log("CLICKED!");
     document.body.parentNode.style.animation = "flash 0.5s";
 });
 
-document.body.parentNode.addEventListener("animationend", function(){
+//after flash print polaroid
+camera.addEventListener("animationend", function(){
     console.log("ended!");
-    polaroid.style.animation = "print 0.5s 1 forwards";
+    camera.style.animation = "print 0.5s 1 forwards";
+    // picture.style.animation = "develop 3s forwards";
 });
